daf58e31993e39b763b951d8a3d9dd5f8a7d82c3
Artu Reis <120587481+Artu-Dev@users.noreply.github.com>
2023-09-07 13:21:46 -0300

    navbottom import

--- toxibook/src/App.jsx
+++ toxibook/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import NavBottom from './components/navBottom/NavBottom'
+import { NavBottom } from './components/navBottom/NavBottom'
 import { PostProvider } from './contexts/PostContext'
 import { AuthUserProvider } from './contexts/AuthUser'
 
