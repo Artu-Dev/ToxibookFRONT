9b1c81c52036a65e0cf1366333029e6f8a5730d9
Artu Reis <120587481+Artu-Dev@users.noreply.github.com>
2023-09-09 19:08:20 -0300

    added navbar on post detail

--- toxibook/src/pages/Home/PostDetails/PostDetails.jsx
+++ toxibook/src/pages/Home/PostDetails/PostDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import PostCard from "../../../components/Cards/PostCard/PostCard"
 
@@ -10,19 +10,21 @@ import "./PostDetails.css"
 import { AlertBox } from "../../../components/AlertBox/AlertBox";
 import {  MdLock } from "react-icons/md";
 import {NavSide} from "../../../components/NavSide/NavSide";
-import { useUserContext } from "../../../contexts/AuthUser";
+import toxibookLogo from "../../../img/toxibookLogo.webp";
 import { renderPosts } from "../../../functions/globalFunctions";
 import { usePostContext } from "../../../contexts/PostContext";
+import { HiArrowLeft } from "react-icons/hi2";
 
 const PostDetails = () => {
   const [post, setPost] = useState(null);
-  // const [comments, setComments] = useState(null);
   const [isLiked, setIsLiked] = useState(false);
   const [loading, setLoading] = useState(true);
 
   const { id } = useParams("id");
   const token = localStorage.getItem("AuthToken");
   const {posts: comments, setPost: setComments} = usePostContext();
+
+  const redirect = useNavigate();
 
   useEffect(() => {
     async function fetchPostById() {
@@ -49,28 +51,41 @@ const PostDetails = () => {
   return (
     <div className="PostDetails-Container centerFlex">
       <NavSide/>
-      <PostCard 
-        liked={isLiked}
-        post={post}
-        permissions={post.permissions}
-        postUser={post.user}
-      />
-      {post?.permissions.canComment ?
-        <CreatePost 
-          type={"reply"}
-          isCommentOf={post.user.tag}
-          isCommentOfID={post._id}
+
+      <div className="simpleNavBar">
+        <div>
+          <div className="backButton" onClick={() => redirect("/")}>
+            <HiArrowLeft/>
+          </div>
+          <img className="toxibookLogo" src={toxibookLogo} alt="toxibook logo" />
+        </div>
+      </div>
+      
+      <div className="posts-container">
+        <PostCard 
+          liked={isLiked}
+          post={post}
+          permissions={post.permissions}
+          postUser={post.user}
         />
-        :
-        <AlertBox
-          theme={"alert"}
-          icon={<MdLock/>}
-          text={"Uh-oh! Parece que os comentários estão desativados pelo autor deste post."}
-        />
-      }
-      {post?.permissions.canComment &&
-        renderPosts(comments, loading, null, true)
-      }
+        {post?.permissions.canComment ?
+          <CreatePost 
+            type={"reply"}
+            isCommentOf={post.user.tag}
+            isCommentOfID={post._id}
+          />
+          :
+          <AlertBox
+            theme={"alert"}
+            icon={<MdLock/>}
+            text={"Uh-oh! Parece que os comentários estão desativados pelo autor deste post."}
+          />
+        }
+        {post?.permissions.canComment &&
+          renderPosts(comments, loading, null, true)
+        }
+        
+      </div>
     </div>
   )
 }
