0714da34a76691e4aa935e98922889f1fb1f749d
Artu Reis <120587481+Artu-Dev@users.noreply.github.com>
2025-02-06 15:48:47 -0300

    Update post.services.js

--- toxibook/src/services/post.services.js
+++ toxibook/src/services/post.services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = "https://toxibook-backend.onrender.com"
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:1234";
+const baseURL = "https://toxibook-backend.onrender.com"
+// const baseURL = process.env.REACT_APP_API_URL || "http://localhost:1234";
 
 
 
